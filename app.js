6793d058cc9d1cff4af8dfb2f9f17f89d5e7b631
Vladimir Riz <resin.fhp@gmail.com>
2021-03-23 19:20:35 +0300

    feat: add ACAO middleware to the server

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const app = express();
 
 app.use(express.json()); // application/json
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use('/feed', feedRouts);
 
 app.listen(8080);
