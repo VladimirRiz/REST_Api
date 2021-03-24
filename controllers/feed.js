4432c9115df449cc71e0593f844d451bb233985c
Vladimir Riz <resin.fhp@gmail.com>
2021-03-24 13:09:17 +0300

    feat: fetch lists of posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,11 +1,23 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Title', content: 'This is first content' }],
+    posts: [
+      {
+        _id: 12,
+        title: 'First Title',
+        content: 'This is first content',
+        imageUrl: 'images/book.png',
+        creator: {
+          name: 'Riz',
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
   const { title, content } = req.body;
+  console.log(title, content);
   res.status(201).json({
     message: 'Success!',
     post: {
